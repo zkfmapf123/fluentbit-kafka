1ebc88ea2716ec8ba47015f63d163e6fef88282d
leedonggyu <zkfmapf123@naver.com>
2025-10-18 13:35:25 +0900

    update kafka up

---
+++ infrastructure/kafka.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+## sudo chown -R 1000:1000 logs/kafka-*
+
+EXTERNAL_UP=$(curl ifconfig.me)
+
+export DOCKER_HOST_IP=$EXTERNAL_UP
+
+docker-compose -f docker-compose.kafka.yml -up --build -d
